5e3182396cbbe3d3071eb117c3d74c10b6fdf06e
kyton61 <sanukizuki@yahoo.co.jp>
2023-02-27 21:56:13 +0900

    初回コミット

--- README.md
+++ README.md
@@ -1,72 +1,9 @@
 
 
-
-## Streaming Replication設定
-### standbyサーバ（ec2-postgres-2-cf）にて以下のコマンドを実行
-primaryサーバにアクセスしてレプリケーションスロットを作成
-```
-sudo su - postgres
-psql -h ec2-postgres-1-cf -U postgres
-## パスワードを入力：password
-select * from pg_create_physical_replication_slot('db02_repl_slot');
-select slot_name, slot_type, active, wal_status from pg_replication_slots;
-\q
-exit
-```
-
-primaryサーバのデータコピー
-```
-sudo -iu postgres pg_basebackup --pgdata /var/lib/pgsql/14/data --format=p \
---write-recovery-conf --checkpoint=fast --label=mffb --progress \
---host=ec2-postgres-1-cf --port=5432 --username=repl
-```
-
-レプリケーション設定
-```
-cat <<EOF >> /var/lib/pgsql/14/data/postgresql.conf
-# Standby
-primary_conninfo = 'user=repl port=5432 host=ec2-postgres-1-cf application_name=db02.repl'
-primary_slot_name = 'db02_repl_slot'
-EOF
-```
-
-レプリケーション開始
-```
-systemctl start postgresql-14
-```
-
-
 ## 動作確認
-### primaryサーバ（ec2-postgres-1-cf）にて以下のコマンドを実行
-データベースの作成とデータインサート
+### ブラウザからstandbyサーバ（ec2-postgres-2-cf）のグローバルIPアドレスにアクセス
 ```
-su - postgres
-createuser -U postgres -d -P testuser
-## パスワードを2回入力
-createdb -U testuser testdb
-psql -U testuser testdb
-testdb=# create table test_table (id int);
-testdb=# insert into test_table values (1);
-testdb=# select * from test_table;
+http://"グローバルIP"//pg_stats_reporter/pg_stats_reporter.php
 ```
-
-レプリケーション状態確認
-```
-\x
-select * from pg_stat_replication;
-```
-
-### standbyサーバ（ec2-postgres-2-cf）にて以下のコマンドを実行
-primaryサーバの情報が反映されていることを確認
-```
-sudo su - postgres
-psql -U testuser testdb
-testdb=# select * from test_table;
-```
-
-データの更新はできない
-```
-testdb=# insert into test_table values (2);
-ERROR:  cannot execute INSERT in a read-only transaction
-```
+スナップショットデータが出力されることを確認する
 
--- setup_primary.sh
+++ setup_primary.sh
@@ -16,9 +16,9 @@ dnf install -y perl-libs # perl-libs is needed by postgresql-contrib
 
 # その他postgreとその周辺ツールのrpmをダウンロード
 ## postgre本体で必要なパッケージ
-wget --tries=3 http://54.169.224.98/centos/8-stream/BaseOS/x86_64/os/Packages/libxslt-1.1.32-6.el8.x86_64.rpm
-wget --tries=3 http://54.169.224.98/centos/8-stream/BaseOS/x86_64/os/Packages/lz4-1.8.3-3.el8_4.x86_64.rpm
-wget --tries=3 http://54.169.224.98/centos/8-stream/BaseOS/x86_64/os/Packages/libicu-60.3-2.el8_1.x86_64.rpm
+wget --tries=3 http://mirror.centos.org/centos/8-stream/BaseOS/x86_64/os/Packages/libxslt-1.1.32-6.el8.x86_64.rpm
+wget --tries=3 http://mirror.centos.org/centos/8-stream/BaseOS/x86_64/os/Packages/lz4-1.8.3-3.el8_4.x86_64.rpm
+wget --tries=3 http://mirror.centos.org/centos/8-stream/BaseOS/x86_64/os/Packages/libicu-60.3-2.el8_1.x86_64.rpm
 ## postgre本体
 wget --tries=3 https://download.postgresql.org/pub/repos/yum/14/redhat/rhel-8.4-x86_64/postgresql14-14.0-1PGDG.rhel8.x86_64.rpm
 wget --tries=3 https://download.postgresql.org/pub/repos/yum/14/redhat/rhel-8.4-x86_64/postgresql14-server-14.0-1PGDG.rhel8.x86_64.rpm
@@ -33,29 +33,6 @@ wget --tries=3 https://github.com/ossc-db/pg_store_plans/releases/download/1.6.1
 wget --tries=3 https://github.com/ossc-db/pg_bulkload/releases/download/VERSION3_1_19/pg_bulkload-3.1.19-1.pg14.rhel8.x86_64.rpm
 wget --tries=3 https://download.postgresql.org/pub/repos/yum/14/redhat/rhel-8.4-x86_64/pg_repack_14-1.4.7-1.rhel8.x86_64.rpm
 wget --tries=3 https://download.postgresql.org/pub/repos/yum/14/redhat/rhel-8-x86_64/orafce_14-3.18.1-1.rhel8.x86_64.rpm
-## postgre周辺ツール（pg_stats_reporter関連）
-wget --tries=3 https://github.com/ossc-db/pg_stats_reporter/releases/download/14.0/pg_stats_reporter-14.0-1.el8.noarch.rpm
-wget --tries=3 http://54.169.224.98/centos/8-stream/AppStream/x86_64/os/Packages/apr-1.6.3-12.el8.x86_64.rpm 
-wget --tries=3 http://54.169.224.98/centos/8-stream/AppStream/x86_64/os/Packages/apr-util-openssl-1.6.1-6.el8.x86_64.rpm 
-wget --tries=3 http://54.169.224.98/centos/8-stream/AppStream/x86_64/os/Packages/apr-util-bdb-1.6.1-6.el8.x86_64.rpm 
-wget --tries=3 http://54.169.224.98/centos/8-stream/AppStream/x86_64/os/Packages/apr-util-1.6.1-6.el8.x86_64.rpm 
-wget --tries=3 http://54.169.224.98/centos/8-stream/AppStream/x86_64/os/Packages/php-7.2.24-1.module_el8.2.0+313+b04d0a66.x86_64.rpm 
-wget --tries=3 http://54.169.224.98/centos/8-stream/AppStream/x86_64/os/Packages/php-common-7.2.24-1.module_el8.2.0+313+b04d0a66.x86_64.rpm 
-wget --tries=3 http://54.169.224.98/centos/8-stream/AppStream/x86_64/os/Packages/php-fpm-7.2.24-1.module_el8.2.0+313+b04d0a66.x86_64.rpm 
-wget --tries=3 http://54.169.224.98/centos/8-stream/AppStream/x86_64/os/Packages/php-intl-7.2.24-1.module_el8.2.0+313+b04d0a66.x86_64.rpm 
-wget --tries=3 http://54.169.224.98/centos/8-stream/AppStream/x86_64/os/Packages/php-cli-7.2.24-1.module_el8.2.0+313+b04d0a66.x86_64.rpm 
-wget --tries=3 http://54.169.224.98/centos/8-stream/AppStream/x86_64/os/Packages/php-xml-7.2.24-1.module_el8.2.0+313+b04d0a66.x86_64.rpm 
-wget --tries=3 http://54.169.224.98/centos/8-stream/AppStream/x86_64/os/Packages/php-pdo-7.2.24-1.module_el8.2.0+313+b04d0a66.x86_64.rpm 
-wget --tries=3 http://54.169.224.98/centos/8-stream/AppStream/x86_64/os/Packages/php-pgsql-7.2.24-1.module_el8.2.0+313+b04d0a66.x86_64.rpm 
-wget --tries=3 http://54.169.224.98/centos/8-stream/AppStream/x86_64/os/Packages/mod_http2-1.15.7-3.module_el8.4.0+778+c970deab.x86_64.rpm 
-wget --tries=3 http://54.169.224.98/centos/8-stream/AppStream/x86_64/os/Packages/libpq-13.3-1.el8_4.x86_64.rpm 
-wget --tries=3 http://54.169.224.98/centos/8-stream/AppStream/x86_64/os/Packages/httpd-tools-2.4.37-43.module_el8.5.0+1022+b541f3b1.x86_64.rpm 
-wget --tries=3 http://54.169.224.98/centos/8-stream/AppStream/x86_64/os/Packages/httpd-filesystem-2.4.37-43.module_el8.5.0+1022+b541f3b1.noarch.rpm 
-wget --tries=3 http://54.169.224.98/centos/8-stream/AppStream/x86_64/os/Packages/httpd-2.4.37-43.module_el8.5.0+1022+b541f3b1.x86_64.rpm 
-wget --tries=3 http://54.169.224.98/centos/8-stream/AppStream/x86_64/os/Packages/libxslt-devel-1.1.32-6.el8.x86_64.rpm 
-wget --tries=3 http://54.169.224.98/centos/8-stream/AppStream/x86_64/os/Packages/nginx-filesystem-1.14.1-9.module_el8.0.0+1060+3ab382d3.noarch.rpm 
-wget --tries=3 http://54.169.224.98/centos/8-stream/BaseOS/x86_64/os/Packages/mailcap-2.1.48-3.el8.noarch.rpm 
-wget --tries=3 http://54.169.224.98/centos/8-stream/AppStream/x86_64/os/Packages/centos-logos-httpd-85.8-2.el8.noarch.rpm 
 
 
 # postgreと周辺ツールのインストール
@@ -77,28 +54,6 @@ pg_store_plans14-1.6.1-1.el8.x86_64.rpm \
 pg_bulkload-3.1.19-1.pg14.rhel8.x86_64.rpm \
 pg_repack_14-1.4.7-1.rhel8.x86_64.rpm \
 orafce_14-3.18.1-1.rhel8.x86_64.rpm
-## postgre周辺ツール（pg_stats_reporter関連）
-rpm -ivh pg_stats_reporter-14.0-1.el8.noarch.rpm \
-apr-1.6.3-12.el8.x86_64.rpm \
-apr-util-openssl-1.6.1-6.el8.x86_64.rpm \
-apr-util-bdb-1.6.1-6.el8.x86_64.rpm \
-apr-util-1.6.1-6.el8.x86_64.rpm \
-php-7.2.24-1.module_el8.2.0+313+b04d0a66.x86_64.rpm \
-php-common-7.2.24-1.module_el8.2.0+313+b04d0a66.x86_64.rpm \
-php-fpm-7.2.24-1.module_el8.2.0+313+b04d0a66.x86_64.rpm \
-php-intl-7.2.24-1.module_el8.2.0+313+b04d0a66.x86_64.rpm \
-php-cli-7.2.24-1.module_el8.2.0+313+b04d0a66.x86_64.rpm \
-php-xml-7.2.24-1.module_el8.2.0+313+b04d0a66.x86_64.rpm \
-php-pdo-7.2.24-1.module_el8.2.0+313+b04d0a66.x86_64.rpm \
-php-pgsql-7.2.24-1.module_el8.2.0+313+b04d0a66.x86_64.rpm \
-mod_http2-1.15.7-3.module_el8.4.0+778+c970deab.x86_64.rpm \
-libpq-13.3-1.el8_4.x86_64.rpm \
-httpd-tools-2.4.37-43.module_el8.5.0+1022+b541f3b1.x86_64.rpm \
-httpd-filesystem-2.4.37-43.module_el8.5.0+1022+b541f3b1.noarch.rpm \
-httpd-2.4.37-43.module_el8.5.0+1022+b541f3b1.x86_64.rpm \
-mailcap-2.1.48-3.el8.noarch.rpm \
-centos-logos-httpd-85.8-2.el8.noarch.rpm \
-nginx-filesystem-1.14.1-9.module_el8.0.0+1060+3ab382d3.noarch.rpm
 
 
 
@@ -131,7 +86,9 @@ shared_preload_libraries = 'pg_statsinfo,pg_stat_statements,pg_store_plans'
 # クエリ実行計画取得のためpg_store_plans追加
 track_counts = 'on'                             # データベースの活動に関する統計情報の取集設定
 track_activities = 'on'                         # セッションで実行中のコマンドに関する情報収集
-pg_statsinfo.snapshot_interval = 10min          # スナップショットの取得間隔
+pg_statsinfo.repository_server = 'host=ec2-postgres-2-cf port=5432 user=postgres dbname=postgres' 
+                                                # スナップショットの保存先
+pg_statsinfo.snapshot_interval = 1min          # スナップショットの取得間隔
 pg_statsinfo.enable_maintenance = 'on'          # 自動メンテナンス設定
 pg_statsinfo.maintenance_time = '00:02:00'      # 自動メンテナンス実行時刻設定
 pg_statsinfo.repolog_min_messages = disable     # ログ蓄積機能の設定
@@ -150,52 +107,23 @@ EOF
 ## set pg_hba.conf
 sed -i '1s/^/local   all             postgres                                ident\n/' /var/lib/pgsql/14/data/pg_hba.conf  
 
-# streaming replication設定
-## Set postgresql.conf
-cat <<EOF >> /var/lib/pgsql/14/data/postgresql.conf
-wal_level = replica
-max_wal_senders = 10
-wal_keep_size = '1GB'
-wal_compression = on
-EOF
-## set pg_hba.conf
-cat <<EOF >> /var/lib/pgsql/14/data/pg_hba.conf
-host    all          all   10.5.10.0/24     trust
-host    replication  repl  10.5.10.0/24   trust
-EOF
-## replication用ユーザの追加
-sudo -iu postgres createuser -U postgres --replication repl
 
 
-# pg_rman用設定
-## アーカイブディレクトリ作成
-mkdir -p /var/lib/pgsql/14/arch
-chmod 700 /var/lib/pgsql/14/arch
-chown postgres:postgres /var/lib/pgsql/14/arch
-## pg_rman動作確認用にアーカイブログモードに設定
-cat <<EOF >> /var/lib/pgsql/14/data/postgresql.conf
-archive_mode = on
-archive_command = 'cp %p /var/lib/pgsql/14/arch/%f'
-archive_timeout = 60
-EOF
 
 ## 追加ライブラリ読み込み
 sudo -iu postgres psql -d postgres -c "CREATE EXTENSION pg_stat_statements"
 sudo -iu postgres psql -d postgres -c "CREATE EXTENSION pg_store_plans"
 
+# パスワードファイル作成
+sudo -iu postgres touch /var/lib/pgsql/.pgpass
+sudo -iu postgres echo "ec2-postgres-2-cf:5432:postgres:postgres:password" > /var/lib/pgsql/.pgpass
+chmod 600 /var/lib/pgsql/.pgpass
 
-# pg_stats_reporterの初期セットアップ
-## set repository db
-sed -i s/'host = dbserver01'/'host = localhost'/g /etc/pg_stats_reporter.ini
-sed -i s/';password ='/'password = password'/g /etc/pg_stats_reporter.ini
+
 # change SELinux for DB
 # https://dev.classmethod.jp/articles/redhat-selinx-might-block-network-connection-to-servers-from-apache-php/ 
-# sudo setsebool -P httpd_can_network_connect_db=1 
-# sudo chcon -h system_u:httpd_sys_rw_content_t /var/www/pg_stats_reporter_lib/{cache,compiled} 
 sed -i s/'SELINUX=enforcing'/'SELINUX=disabled'/g /etc/selinux/config
-# sudo systemctl start httpd.service
-systemctl enable httpd.service
-systemctl start httpd.service
+
 
 # hostname設定のため再起動
 shutdown -r now
